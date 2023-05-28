d65752ec0f9dec1368b26fddf2bcb8b6943a5aa2
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-05-28 16:01:10 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/Untitled1/index.js
@@ -0,0 +1,77 @@
+import { Text } from "react-native";
+import React from "react";
+import { StyleSheet, ScrollView, SafeAreaView } from "react-native";
+
+const Untitled1 = () => {
+  return <SafeAreaView style={styles.safeArea}>
+        <ScrollView contentContainerStyle={{
+      backgroundColor: '#f0f0f1',
+      padding: 10,
+      position: 'relative',
+      flex: 1
+    }}><Text style={styles.EzkLTXZp}>{"Select User Type"}</Text><Text style={styles.xavCelRr}>{"Login"}</Text><Text style={styles.cahvTMHI}>{"Sign Up For Free"}</Text><Text style={styles.tgkpTkyq}>{"About us"}</Text><Text style={styles.GjLdfHxW}>{"FAQ"}</Text><Text style={styles.RMbOdBoo}>{" Search Bar "}</Text><Text style={styles.lFlMFWXh}>{"Contact us"}</Text><Text style={styles.TKvXLbSW}>{"Dashboard"}</Text></ScrollView>
+    </SafeAreaView>;
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    height: '100%'
+  },
+  EzkLTXZp: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  xavCelRr: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  cahvTMHI: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  tgkpTkyq: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  GjLdfHxW: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  RMbOdBoo: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  lFlMFWXh: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  },
+  TKvXLbSW: {
+    width: 100,
+    height: 50,
+    lineHeight: 14,
+    fontSize: 14,
+    borderRadius: 0
+  }
+});
+export default Untitled1;
