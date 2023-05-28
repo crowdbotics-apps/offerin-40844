a105da2cef18d0622dcdcc5ebf3481c83cdeaa46
iniobong Etuk <iniobong.etuk@crowdbotics.com>
2023-05-28 22:17:07 +0100

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

---
+++ screens/ScreenAI7/index.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+
+const SignUpScreen = () => {
+  return <View style={styles.container}>
+      <Text style={styles.title}>Sign up For Free</Text>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.input} placeholder="First & Last Name" />
+        <TextInput style={styles.input} placeholder="Email" />
+        <TextInput style={styles.input} placeholder="Create password" secureTextEntry={true} />
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity style={styles.checkbox} />
+          <Text style={styles.checkboxText}>I have read Terms and Conditions and Privacy Policy</Text>
+        </View>
+        <View style={styles.checkboxContainer}>
+          <TouchableOpacity style={styles.checkbox} />
+          <Text style={styles.checkboxText}>Remember me</Text>
+        </View>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Sign Up For Free</Text>
+      </TouchableOpacity>
+    </View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff'
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 30
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 20
+  },
+  checkboxContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20
+  },
+  checkbox: {
+    width: 20,
+    height: 20,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginRight: 10
+  },
+  checkboxText: {
+    fontSize: 16
+  },
+  button: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 15,
+    paddingHorizontal: 30,
+    borderRadius: 5
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+});
+export default SignUpScreen;
